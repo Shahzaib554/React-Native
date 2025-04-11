0d11eb0e028f1549cedc3d3f9fb3b5ebae6dc67b
Shahzaib554 <86713644+Shahzaib554@users.noreply.github.com>
2025-04-11 13:05:38 -0700

    first commit

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Button, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize: 30}}>React Native App Discription!</Text>
+      <Text style={{fontSize: 25}}>I Learning React Native!</Text>
+      <Button title="Press Here"/>
+    </View>
+  );
+};
+
+export default App;
