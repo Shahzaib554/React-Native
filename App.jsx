8c8c75037cb5c7e5bd705bd392ae69b718adf209
Shahzaib554 <86713644+Shahzaib554@users.noreply.github.com>
2025-05-01 07:30:39 -0700

    New Text Added

--- App.jsx
+++ App.jsx
@@ -4,7 +4,7 @@ import {Button, Text, View} from 'react-native';
 const App = () => {
   return (
     <View>
-      <Text style={{fontSize: 30}}>React Native App Discription!</Text>
+      <Text style={{fontSize: 30}}>My App</Text>
       <Text style={{fontSize: 25}}>I Learning React Native!</Text>
       <Text>Uploaded On GitHub</Text>
       <Button title="Press Here"/>
