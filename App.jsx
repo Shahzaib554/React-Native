76a0afcd49ee5b0f306968e95fcd48d0bd9da844
Shahzaib554 <86713644+Shahzaib554@users.noreply.github.com>
2025-05-07 13:25:32 -0700

    Add New Component Screen.

--- App.jsx
+++ App.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView, Text, View } from 'react-native';
 import PostListScreen from '../React-Native/src/screens/postListScreen';
-
+import JSX from './src/components/JSX';
 
 const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-        <PostListScreen />
-    </QueryClientProvider>
+    // <QueryClientProvider client={queryClient}>
+    //     <PostListScreen />
+    // </QueryClientProvider>
+
+  <View>
+    <JSX/>
+  </View>
   );
 };
 
---
+++ src/components/JSX.jsx
@@ -0,0 +1,21 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const JSX = () => {
+
+    let name = 'Shahzaib'
+    const age = () => {
+        return 25
+    }
+    return (
+
+    <View>
+      <Text style= {{fontSize: 30}}>JSX</Text>
+      <Text style={{fontSize: 30}}>{name}</Text>
+      <Text style={{fontSize: 30}}>{2+2}</Text>
+      <Text style={{fontSize: 30}}>{age()}</Text>
+    </View>
+  )
+}
+
+export default JSX
